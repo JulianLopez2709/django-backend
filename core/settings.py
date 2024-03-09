84e596aa5366bca9c0c7a931ba57b5a7df29850e
JulianLopez2709 <judalomor27.95@gmail.com>
2024-03-08 21:18:37 -0500

    allow host

--- core/settings.py
+++ core/settings.py
@@ -130,3 +130,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
+
+ALLOWED_HOSTS = ["https://django-backend-production.up.railway.app/"]
